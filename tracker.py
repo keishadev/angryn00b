72d5da16dd9ebc88d6dc4cf157559cd545e22891
keishaatech <36748277+keishaatech@users.noreply.github.com>
2018-03-25 17:58:33 +0700

    angryn00b
    h4h4h4hh4

---
+++ tracker.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# Coded By GeoMetriX
+# Thanks To clown hacktivism & api access
+import os
+import urllib2
+import json
+
+os.system("cls")
+os.system("title CLOWN HACKTIVISM")
+print("            #####################################################")
+print("            #                                                   #")
+print("            #               GEOMETRIX IP TRACKER                #")
+print("            #                 cl0wn hacktivism                  #")
+print("            #          http://clownhacktivismteam.org           #")
+print("            #                                                   #")
+print("            #####################################################")
+print("")
+
+while True:
+		ip=raw_input("Your Target ip:")
+		url = "http://ip-api.com/json/"
+		response = urllib2.urlopen(url + ip)
+		data = response.read()
+		values = json.loads(data)
+
+		
+		print("")
+		print("")
+		print("")
+		print("+============[Tracked]============+")
+		print(" IP: " + values['query'])
+		print(" City: " + values['city'])
+		print(" As: " + values['as'])
+		print(" Country: " + values['country'])
+		print(" Status: " + values['status'])
+		print(" Region: " + values['region'])
+		print(" Time Zone: " + values['timezone'])
+		print("+============[Tracked]============+")
+		print("")
+		print(" Thanks for ip-api.com for api access!")
+
+		
+		break
+
+##############################################################################
